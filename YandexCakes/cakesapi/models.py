48a98a7329411501446e868f0cf702d16eb9d948
Daniil <ksusonic@yandex.ru>
2021-03-20 00:05:52 +0300

    Done with models and added DRF serializers.py file

--- YandexCakes/cakesapi/models.py
+++ YandexCakes/cakesapi/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Region(models.Model):
+    region = models.IntegerField()
+
+
+class WorkingHours(models.Model):
+    working_hours = models.CharField(max_length=11)
+
+    # TODO parse time from model
+
+
+class Courier(models.Model):
+    courier_id = models.AutoField(primary_key=True)
+
+    FOOT = 'foot'
+    BIKE = 'bike'
+    CAR = 'car'
+    COURIER_TYPES = [  # Тип <-> грузоподъемность
+        (FOOT, '10'),
+        (BIKE, '15'),
+        (CAR, '50')
+    ]
+    courier_type = models.CharField(
+        max_length=4,
+        choices=COURIER_TYPES,
+    )
+
+    regions = models.ManyToManyField(Region)
+    working_hours = models.ManyToManyField(WorkingHours)
