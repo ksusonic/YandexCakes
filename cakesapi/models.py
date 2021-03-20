2b221ff40b3d130bf67ad92e9596d739e39b8f5e
Daniil <ksusonic@yandex.ru>
2021-03-21 00:05:01 +0300

    Courier database model

--- cakesapi/models.py
+++ cakesapi/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 
-# class Region(models.Model):
-#     region = models.IntegerField()
-#
-#
-# class WorkingHours(models.Model):
-#     working_hours = models.CharField(max_length=11)
-#
-#     # TODO parse time from model
+class Region(models.Model):
+    region = models.IntegerField()
+
+
+class WorkingHours(models.Model):
+    working_hours = models.CharField(max_length=11)
+
+    # TODO parse time from model
 
 
 class Courier(models.Model):
@@ -26,8 +26,5 @@ class Courier(models.Model):
         choices=COURIER_TYPES,
     )
 
-    regions = models.IntegerField()  # models.ManyToManyField(Region)
-    working_hours = models.CharField(max_length=11)  # models.ManyToManyField(WorkingHours)
-
-    def __str__(self):
-        return self.courier_id
+    regions = models.ManyToManyField(Region)
+    working_hours = models.ManyToManyField(WorkingHours)
