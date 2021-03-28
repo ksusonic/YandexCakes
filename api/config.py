376f8af807fbe48233b0859e3bab4ef094fd5e66
Daniil <ksusonic@yandex.ru>
2021-03-28 20:50:21 +0300

    Переход на FastApi: удобство работы с Pydantic, SwaggerUI и избавлением от лишних зависимостей, как во Flask.

--- api/config.py
+++ api/config.py
@@ -1,5 +1,9 @@
-SECRET_KEY = "hello_Alice"
-SQLALCHEMY_DATABASE_URI = 'sqlite:db.sqlite'
+SQLALCHEMY_DATABASE_URI = 'sqlite:db/db.sqlite'  # TODO: Replace on PostgreSQL and get from environment
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLAlCHEMY_ECHO = False
+
 DEBUG = True
+
+desc = """Чтобы немного скрасить жизнь на самоизоляции, представляем вам интернет-магазин по доставке конфет \
+"Сласти от всех напастей". Это REST API сервис, который позволяет нанимать курьеров на работу, \
+принимать заказы и оптимально распределять заказы между курьерами, попутно считая их рейтинг и заработок."""
--- api/main.py
+++ api/main.py
@@ -1,9 +1,15 @@
+from uvicorn import run
 from fastapi import FastAPI
 
-app = FastAPI()
+from api.config import desc
+
+app = FastAPI(title="YandexCakes", description=desc)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
+if __name__ == '__main__':
+    run(app, host='0.0.0.0', port=8080)
