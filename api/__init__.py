4a9012b6da69bb1938f807e7687e0012fa4f68ec
Daniil <ksusonic@yandex.ru>
2021-03-28 17:37:05 +0300

    Flask minimal template

---
+++ api/__init__.py
@@ -0,0 +1,20 @@
+"""
+Сервис с REST API.
+Интернет-магазин по доставке конфет "Сласти от всех напастей".
+Вступительное задание в Школу бэкенд‑разработки Яндекса 2021.
+"""
+__author__ = 'Daniil Moiseev'
+__maintainer__ = __author__
+
+__email__ = 'ksusonic@yandex.ru'
+__license__ = 'MIT'
+__version__ = '0.0.1'
+
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+)
